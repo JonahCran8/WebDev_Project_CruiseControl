f94b224e28b9da41b57f40634fb605ea37dc8437
JonahCran8 <C24356146@myTUDublin.ie>
2024-12-02 19:27:15 +0000

    update

--- Javascript/contact.js
+++ Javascript/contact.js
@@ -1,4 +1,14 @@
 function submitForm() {
-    alert("Submitted");
-    document.getElementById("contact_form").reset();
+    const email = document.getElementById("email").value
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailPattern.test(email)) {
+        alert("Submitted");
+        document.getElementById("contact_form").reset();
+    }
+
+    else {
+        alert("Email not valid")
+    }
+
 }
